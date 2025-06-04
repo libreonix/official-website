7dcd42484fc08c1a018b9a0d06794163726b548b
akshat k <inquiry.akshatkotpalliwar@gmail.com>
2025-06-04 20:18:27 +0530

    robots - conf

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,17 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import Careers from "./pages/Careers";
+import Blogs from "./pages/Blogs";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
+import Investors from "./pages/Investors";
+import Partners from "./pages/Partners";
+import FAQ from "./pages/FAQ";
+import Ticket from "./pages/Ticket";
+import TOS from "./pages/TOS";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import About from "./pages/About";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +27,17 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/investors" element={<Investors />} />
+          <Route path="/partners" element={<Partners />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/ticket" element={<Ticket />} />
+          <Route path="/tos" element={<TOS />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
